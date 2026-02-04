fcf48c114e1edd4be6c7c074bf77a546ac41277f
Aravindh <54079536+aravindh28@users.noreply.github.com>
2026-02-03 19:38:53 -0800

    [1045] [Time Beats: 69.50%] - LeetPush

--- sql_problems/Customers-Who-Bought-All-Products.sql
+++ sql_problems/Customers-Who-Bought-All-Products.sql
@@ -1,12 +1,8 @@
 1-- Write your PostgreSQL query statement below
 2
-3WITH A as (
-4SELECT
-5    customer_id,
-6    COUNT(DISTINCT product_key) as count
-7FROM
-8    Customer
-9GROUP BY customer_id
-10)
-11SELECT customer_id from A
-12where count = (SELECT COUNT(product_key) from Product)
+3SELECT
+4    customer_id
+5FROM
+6    Customer
+7GROUP BY customer_id
+8HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) from Product)
